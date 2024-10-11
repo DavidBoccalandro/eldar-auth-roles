5c37d37498eff19b3e1d363fb68dd37b6a5654ba
David <davidboccalandro@hotmail.com>
2024-10-11 18:27:50 -0300

    add creation button to admin role

--- src/app/features/posts/posts-list/posts-list.component.ts
+++ src/app/features/posts/posts-list/posts-list.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { selectUserRole } from '@app/features/auth/store/auth.selectors';
+import { ModalComponent } from '@app/shared/components/modal/modal.component';
 import { Post } from '@app/shared/models/post.model';
 import { UserRole } from '@app/shared/models/user.model';
 import { Store } from '@ngrx/store';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { Observable } from 'rxjs';
 import { PostsActions } from '../store/actions/posts.actions';
 import { selectAllPosts } from '../store/posts.selectors';
@@ -13,13 +16,21 @@ import { selectAllPosts } from '../store/posts.selectors';
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule],
+  imports: [
+    CommonModule,
+    CardModule,
+    ButtonModule,
+    ModalComponent,
+    InputTextModule,
+    InputTextareaModule,
+  ],
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss'],
 })
 export class PostsListComponent implements OnInit {
   posts$!: Observable<Post[]>;
   userRole$!: Observable<UserRole | undefined>;
+  UserRoleEnum = UserRole;
 
   constructor(private store: Store) {}
 
